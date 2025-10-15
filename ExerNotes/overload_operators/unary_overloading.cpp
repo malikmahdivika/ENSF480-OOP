4d4feba0a80c5b7e45b6ed9c87e316572f316ccd
malikmahdivika <144976344+malikmahdivika@users.noreply.github.com>
2025-10-15 00:32:27 -0600

    unary (and i guess sidetracked with enums) notes complete

--- ExerNotes/overload_operators/unary_overloading.cpp
+++ ExerNotes/overload_operators/unary_overloading.cpp
@@ -4,6 +4,7 @@
  * Examples for slightly more advanced operator overloading; unaries and less conventional operators like [] (which is binary).
 */
 #include <iostream>
+#include <string>
 using namespace std;
 
 // Tarot cards enum
@@ -15,7 +16,8 @@ class Arcana {
         Tarot arcanaInHand[5];      // example array to test [] overloading
         int arcanaLifespan;
     public:
-        Arcana(const Tarot hand[], int size, int arcanaNum);
+        Arcana(const Tarot hand[], int arcanaNum);
+        string toString(Tarot card);
         void print();
 
         Tarot& operator[](int index); // [] overloading
@@ -24,33 +26,63 @@ class Arcana {
 };
 
 // Constructor/Helper funcs
-Arcana::Arcana(const Tarot hand[], int size, int num) : arcanaLifespan(num) {
-    for (int i = 0; i < size; i++) {
+Arcana::Arcana(const Tarot hand[], int num) : arcanaLifespan(num) {
+    for (int i = 0; i < 5; i++) {
         arcanaInHand[i] = hand[i];
     }
 }
 void Arcana::print() {
     cout << "Cards currently in hand: ";
     for (Tarot card : arcanaInHand) {
-        cout << card << ", ";
+        cout << toString(card) << ", ";
     }
     cout << endl;
-    printf("Lifespan of current Arcana: %d", arcanaLifespan);
+    printf("Lifespan of current Arcana: %d\n", arcanaLifespan);
 }
-
-// overloading << for print function
-ostream& operator <<(ostream& os, Tarot card) {
-    switch(card) {
-        case THE_FOOL:  return os << "The Fool";
-        case THE_TOWER:  return os << "The Tower";
-        case THE_HIEROPHANT:  return os << "The Hierophant";
-        case THE_LOVERS:    return os << "The Lovers";
-        case STRENGTH:      return os << "Strength";
-        case THE_HERMIT:    return os << "The Hermit";
-        case THE_HIGH_PRIESTESS:   return os << "The High Priestess";
+// string function
+string Arcana::toString(Tarot card) {
+    switch (card) {
+        case THE_FOOL:          return "The Fool";
+        case THE_TOWER:         return "The Tower";
+        case THE_HIEROPHANT:    return "The Hierophant";
+        case THE_LOVERS:        return "The Lovers";
+        case STRENGTH:          return "Strength";
+        case THE_HERMIT:        return "The Hermit";
+        case THE_HIGH_PRIESTESS:return "The High Priestess";
+        default:                return "Unknown Card";
     }
 }
 // Modifiable non-constant overload
 Tarot& Arcana::operator[](int index) {
     return arcanaInHand[index];
+}
+Arcana& Arcana::operator++() {
+    ++arcanaLifespan;           // increment first
+    return *this;      // return the updated object
+}
+// Postfix ++ (e.g., x++)
+Arcana Arcana::operator++(int) {
+    Arcana temp = *this;  // make a copy (old value)
+    ++arcanaLifespan;               // then increment
+    return temp;           // return old value
+}
+
+int main() {
+    Tarot hand[] = {THE_FOOL, THE_HIGH_PRIESTESS, STRENGTH, THE_FOOL, THE_LOVERS};
+    Arcana ar = Arcana(hand, 2);
+    Arcana ar1 = Arcana(hand, 2);
+    cout << "Initial state" << endl;
+    ar.print();
+
+    ar[0] = THE_HIEROPHANT;
+    cout << "After hand[0] modification" << endl;
+    ar.print();
+
+    // comparing post/pre-fix incrementors
+    for (int i = 0; i < 2; i++) {
+        ar++;
+        ar.print();
+        ++ar1;
+        ar1.print();
+    }
 }
