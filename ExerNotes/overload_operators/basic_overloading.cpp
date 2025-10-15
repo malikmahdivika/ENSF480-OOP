888f2c698a5d12d2886afebeb0d516bf668115f6
malikmahdivika <144976344+malikmahdivika@users.noreply.github.com>
2025-10-14 21:45:12 -0600

    + overloading example

--- ExerNotes/overload_operators/basic_overloading.cpp
+++ ExerNotes/overload_operators/basic_overloading.cpp
@@ -3,6 +3,7 @@
  * Dev Date: Oct. 14, 2025
  * Examples for basic operator overloading (most binary operators essentially)
 */
+#include <iostream>
 
 // Counter example class
 class Counter {
@@ -10,23 +11,28 @@ class Counter {
         int count;
     public:
         Counter(int initval);
-        void increment(int increment_by);
+        void increment();
         void print();
+
+        Counter operator +(Counter const &rhs);
 };
 
 // Implementations
-Counter::Counter(int initval) {
-    count = initval;
-}
-void Counter::increment(int val) {
-    count += val;
-}
+Counter::Counter(int initval) : count(initval) {}
+void Counter::increment() {count++;}
+void Counter::print() {std::cout << count;}
+
+Counter Counter::operator +(const Counter& rhs) {    // member-function operator overloading assumes the calling object is the LHS
+    Counter newCounter(count + rhs.count);           // returns new Counter object, in this case assignment operator is NOT overloaded since
+    return newCounter;                               // the only variable of Counter class is an integer primitive. ("Big 3" is not necessary)
+}                                                    
 
 // Execution tests
 int main() {
     Counter c1 = Counter(5);
     Counter c2 = Counter(5);
-    c1.increment(3);
-
+    c1.increment();
     
+    Counter c3 = c1 + c2;
+    c3.print();
 }
